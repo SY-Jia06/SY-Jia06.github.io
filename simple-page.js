6f22bfa6d6952fb883350fe13835715c25c31f04
Jl <168995738@qq.com>
2026-04-02 20:40:27 +0800

    feat: replace home banner with snowy northern landscape & make about page avatar zoomable via lightbox

--- simple-page.js
+++ simple-page.js
@@ -3,4 +3,7 @@ const { initMobileNav, initScrollEffects } = window.BlogShared;
 document.addEventListener("DOMContentLoaded", () => {
     initMobileNav();
     initScrollEffects();
+    if (window.ImageLightbox) {
+        window.ImageLightbox.mount({ contentElement: document.body });
+    }
 });
